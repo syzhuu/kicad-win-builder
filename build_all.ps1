1b1c0b49b495c2383e684c9b75a2763522871883
Ethan Chien <liangtie.qian@gmail.com>
2024-10-24 21:33:06 +0800

    Update build all stript

--- build_all.ps1
+++ build_all.ps1
@@ -1,5 +1,6 @@
 ./build.ps1 -Config -UseMsvcCmake $False
 ./build.ps1 -Init
 ./build.ps1 -Env -Arch x64
-./build.ps1 -Vcpkg -Latest -Arch x64
 ./build.ps1 -Build -Latest -Arch x64 -BuildType Release
+./build.ps1 -PreparePackage  -Arch x64   -BuildType Release
+./build.ps1 -Package  -PackType Nsis   -Arch x64   -BuildType Release
